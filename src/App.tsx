b8a92c0294f16a0b91594cfd9c96947a22b4eafc
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-04-08 14:03:07 +0100

    Set up Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-pw" element={<ForgotPW />} />
+        <Route path="/set-new-pw" element={<SetNewPassword />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/home-dashboard" element={<HomeDashboard />} /> */}
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+import "./Home.scss";
+
+export default function Home() {
+  return <main className="home">Homepage</main>;
+}
