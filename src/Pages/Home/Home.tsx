9203b6dd541f93bed044dc8b913affd3ec5cf21d
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-04-08 16:09:39 +0100

    Add roboto font to index.html, layout for hero

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,16 @@
 import "./Home.scss";
+import heroImage from "../../assets/images/hero-image.svg";
 
 export default function Home() {
-  return <main className="home">Homepage</main>;
+  return (
+    <article className="home">
+      <article className="hero">
+        <p className="hero__paragraph1">
+          Not getting credit for doing all the housework...?
+        </p>
+        <h1 className="hero__heading">PETTY TRACKER</h1>
+        <img src={heroImage} className="hero__image" alt="Italian Trulli"></img>
+      </article>
+    </article>
+  );
 }
