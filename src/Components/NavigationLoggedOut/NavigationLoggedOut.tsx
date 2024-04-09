288a7059a5cd95beacaafc0b65b84166ef3f33e1
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-04-09 16:25:24 +0100

    Add links to NavigationLoggedOut

--- src/Components/NavigationLoggedOut/NavigationLoggedOut.tsx
+++ src/Components/NavigationLoggedOut/NavigationLoggedOut.tsx
@@ -1,21 +1,26 @@
 import "./NavigationLoggedOut.scss";
 import navLogIn from "../../assets/images/nav-login.svg";
 import navSignUp from "../../assets/images/nav-signup.svg";
+import { Link } from "react-router-dom";
 
 export default function NavigationLoggedOut(): JSX.Element | null | string {
   return (
     <nav className="navigation-logged-out">
       <article className="navigation-logged-out__white">
-        <img
-          src={navLogIn}
-          alt="log-in icon"
-          className="navigation-logged-out__icon"
-        />
-        <img
-          src={navSignUp}
-          alt="log-in icon"
-          className="navigation-logged-out__icon"
-        />
+        <Link to="/login">
+          <img
+            src={navLogIn}
+            alt="log-in icon"
+            className="navigation-logged-out__icon"
+          />
+        </Link>
+        <Link to="/signup">
+          <img
+            src={navSignUp}
+            alt="log-in icon"
+            className="navigation-logged-out__icon"
+          />
+        </Link>
       </article>
     </nav>
   );
